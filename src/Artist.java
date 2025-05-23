b7f7743deec743fd5490efbc34b30d924fa23554
giovannibragasv <giovannibragav@gmail.com>
2025-05-23 13:48:01 -0300

    feat: add basic artist class

---
+++ src/Artist.java
@@ -0,0 +1,57 @@
+import java.util.Date;
+
+public class Artist {
+    private String name;
+    private String genre;
+    private String country;
+    private Date formationDate;
+
+    public Artist(String genre, String country, Date formationDate, String name) {
+        this.genre = genre;
+        this.country = country;
+        this.formationDate = formationDate;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getFormationDate() {
+        return formationDate;
+    }
+
+    public void setFormationDate(Date formationDate) {
+        this.formationDate = formationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "name='" + name + '\'' +
+                ", genre='" + genre + '\'' +
+                ", country='" + country + '\'' +
+                ", formationDate=" + formationDate +
+                '}';
+    }
+}
